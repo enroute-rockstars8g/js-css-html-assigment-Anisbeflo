65992403edde34de15a9a856b230bef8342abcea
Anisbeflo <abejarano0@ucol.mx>
2022-04-06 10:50:23 -0500

    Now fibo show the pokemon of that number

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function fibo() {
   const fiboNumber = parseInt(document.getElementById("inputFibo").value);
   let n1 = 0, n2 = 1, nextTerm;
+  let ns;
 
   for (let i = 1; i < fiboNumber; i++) {
     nextTerm = n1 + n2;
     n1 = n2;
     n2 = nextTerm;
   }
+  if(n1<10){
+    ns="00"+n1;
+  }
+  else if(n1<100){
+    ns="0"+n1;
+  }
+  else{
+    ns=n1.toString();
+  }
   const container = document.getElementById("fiboContainer");
+  let foto = document.getElementById("foto1");
+  foto.src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/"+ns+".png"
   container.innerText = n1;
 }
 
